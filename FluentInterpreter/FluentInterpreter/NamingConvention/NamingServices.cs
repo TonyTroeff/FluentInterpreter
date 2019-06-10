da96ab6758e08308371c3f66a3dc40d925e7318e
TonyTroeff <tony13troev@gmail.com>
2019-06-10 08:28:59 +0300

    Fixed the NamingServices properties.

--- FluentInterpreter/FluentInterpreter/NamingConvention/NamingServices.cs
+++ FluentInterpreter/FluentInterpreter/NamingConvention/NamingServices.cs
@@ -4,11 +4,95 @@ namespace FluentInterpreter.NamingConvention
 
 	public static class NamingServices
 	{
-		public static IForeignKeyNaming ForeignKeyNaming { get; set; } = new DefaultForeignKeyNaming();
-		public static IIndexNaming IndexNaming { get; set; } = new DefaultIndexNaming();
-		public static IColumnNaming ColumnNaming { get; set; } = new DefaultColumnNaming();
-		public static IUniqueIndexNaming UniqueIndexNaming { get; set; } = new DefaultUniqueIndexNaming();
-		public static IPrimaryKeyNaming PrimaryKeyNaming { get; set; } = new DefaultPrimaryKeyNaming();
-		public static ITableNaming TableNaming { get; set; } = new DefaultTableNaming();
+		private static IForeignKeyNaming _foreignKeyNaming = new DefaultForeignKeyNaming();
+		private static IIndexNaming _indexNaming = new DefaultIndexNaming();
+		private static IColumnNaming _columnNaming = new DefaultColumnNaming();
+		private static IUniqueIndexNaming _uniqueIndexNaming = new DefaultUniqueIndexNaming();
+		private static IPrimaryKeyNaming _primaryKeyNaming = new DefaultPrimaryKeyNaming();
+		private static ITableNaming _tableNaming = new DefaultTableNaming();
+
+		public static IForeignKeyNaming ForeignKeyNaming
+		{
+			get
+			{
+				Common.CheckForNull(_foreignKeyNaming);
+				return _foreignKeyNaming;
+			}
+			set
+			{
+				Common.CheckForNull(value);
+				_foreignKeyNaming = value;
+			}
+		}
+
+		public static IIndexNaming IndexNaming
+		{
+			get
+			{
+				Common.CheckForNull(_indexNaming);
+				return _indexNaming;
+			}
+			set
+			{
+				Common.CheckForNull(value);
+				_indexNaming = value;
+			}
+		}
+
+		public static IColumnNaming ColumnNaming
+		{
+			get
+			{
+				Common.CheckForNull(_columnNaming);
+				return _columnNaming;
+			}
+			set
+			{
+				Common.CheckForNull(value);
+				_columnNaming = value;
+			}
+		}
+
+		public static IUniqueIndexNaming UniqueIndexNaming
+		{
+			get
+			{
+				Common.CheckForNull(_uniqueIndexNaming);
+				return _uniqueIndexNaming;
+			}
+			set
+			{
+				Common.CheckForNull(value);
+				_uniqueIndexNaming = value;
+			}
+		}
+
+		public static IPrimaryKeyNaming PrimaryKeyNaming
+		{
+			get
+			{
+				Common.CheckForNull(_primaryKeyNaming);
+				return _primaryKeyNaming;
+			}
+			set
+			{
+				Common.CheckForNull(value);
+				_primaryKeyNaming = value;
+			}
+		}
+
+		public static ITableNaming TableNaming
+		{
+			get
+			{
+				Common.CheckForNull(_tableNaming);
+				return _tableNaming;
+			}
+			set
+			{
+				Common.CheckForNull(value);
+				_tableNaming = value;
+			}
+		}
 	}
 }
