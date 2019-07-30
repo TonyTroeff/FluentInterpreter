c74ba250a21baea18115af2a50095f8129e13960
TonyTroeff <tony13troev@gmail.com>
2019-07-30 16:17:27 +0300

    ADding postgresql support.

--- FluentInterpreter/FluentInterpreter/PropertiesConfiguration/TypesConfiguration/PostgreSqlTypeResolver.cs
+++ FluentInterpreter/FluentInterpreter/PropertiesConfiguration/TypesConfiguration/PostgreSqlTypeResolver.cs
@@ -6,38 +6,35 @@ using System;
 
 namespace FluentInterpreter.PropertiesConfiguration.TypesConfiguration
 {
-    public class PostgreSqlTypeResolver : ITypeResolver
-    {
-        public TypeCollection DefinedTypes { get; } = new TypeCollection
-        {
-            {typeof(byte), "smallint"},
-            {typeof(short), "smallint"},
-            {typeof(int), "integer"},
-            {typeof(long), "bigint"},
-            {typeof(bool), "boolea    n"},
-            {typeof(Guid), "uuid"},
-            {typeof(DateTime), "date"}
-        };
+	public class PostgreSqlTypeResolver : ITypeResolver
+	{
+		public TypeCollection DefinedTypes { get; } = new TypeCollection
+		{
+			{typeof(byte), "smallint"},
+			{typeof(short), "smallint"},
+			{typeof(int), "integer"},
+			{typeof(long), "bigint"},
+			{typeof(bool), "boolean"},
+			{typeof(Guid), "uuid"},
+			{typeof(DateTime), "date"}
+		};
 
-        public string GetStringType(int length = 0, bool isUnicode = true, bool isFixedLength = false)
-        {
-            string type = "";
+		public string GetStringType(int length = 0, bool isUnicode = true, bool isFixedLength = false)
+		{
+			string type = "";
 
-            if (isFixedLength == false) type += "var";
+			if (isFixedLength == false) type += "var";
 
-            type += $"char{(length > 0 ? $"({length.ToString()})" : "")}";
+			type += $"char{(length > 0 ? $"({length.ToString()})" : "")}";
 
-            return type;
-        }
+			return type;
+		}
 
-        public string GetFloatingPointType(int precision = 18, int scale = 0)
-        {
-            return $"decimal({precision}, {scale})";
-        }
+		public string GetFloatingPointType(int precision = 18, int scale = 0)
+		{
+			return $"decimal({precision}, {scale})";
+		}
 
-        public string GetMoneyType()
-        {
-            return "money";
-        }
-    }
+		public string GetMoneyType() { return "money"; }
+	}
 }
